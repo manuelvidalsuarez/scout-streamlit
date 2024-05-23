1d87332f50274ae6992624df7bdc2eb5a916cd65
manuelvidalsuarez <126108086+manuelvidalsuarez@users.noreply.github.com>
2024-05-23 14:23:41 +0200

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,11 +120,12 @@ metricas = st.selectbox('Método de selección de métricas relevantes:', ['Posi
 comparacion = st.selectbox('Tipo de comparación:', ['Posición específica', 'Posición asociada'], index=0)
 
 # Mostrar advertencia si se selecciona "Posición asociada"
-if metricas == 'Posición asociada' or comparacion == 'Posición asociada':
+if metricas == 'Posición asociada' :
     st.warning(
-        "Al seleccionar el método de evaluación asociado, ten en cuenta que las notas pueden variar. "
-        "Este método compara las métricas destacadas de los jugadores no solo con aquellos de su posición específica, "
-        "sino también con jugadores de posiciones asociadas. Esto puede resultar en modificaciones en las notas debido a la comparación más amplia."
+        "Al seleccionar el método de selección de métricas asociado, ten en cuenta que las notas pueden variar. "
+        "En este caso el proceso partiría de los jugadores TOP pero seleccionaría las métricas en las que destacan"
+         "en comparación, no solo con los de su posición si no tambien con los de posiciones asociadas "
+        "Esto puede resultar en modificaciones en las notas debido a unas métricas más dgenéricas."
     )
 
 # Carga de datos
